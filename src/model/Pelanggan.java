cf1a4a3023e73e70c8acf11eea54af4dd08509f7
Vemas <Vemas@LAPTOP-4GNFR4OQ>
2023-06-02 15:39:22 +0700

    Add Pelanggan.java (MODEL)

---
+++ src/model/Pelanggan.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Vemas
+ */
+public class Pelanggan {
+    private int id;
+    private String nama;
+    private String noTelp;
+    private String alamat;
+
+    public Pelanggan(int id, String nama, String noTelp, String alamat) {
+        this.id = id;
+        this.nama = nama;
+        this.noTelp = noTelp;
+        this.alamat = alamat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+    public String showDataPelanggan(){
+        return
+                this.id + " | "
+                + this.nama + " | "
+                + this.noTelp + " | "
+                + this.alamat;
+    }
+    
+    
+}
