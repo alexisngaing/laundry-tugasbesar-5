8cfa2f46e2b76910a617392618aca28219cf7404
Vemas <Vemas@LAPTOP-4GNFR4OQ>
2023-06-02 15:40:28 +0700

    Add DBConnection

--- src/connection/DbConnection.java
+++ src/connection/DbConnection.java
@@ -6,6 +6,33 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 public class DbConnection {
+
+    public static Connection CON;
+    public static final String URL = "jdbc:mysql://";
+    public static final String PATH = "localhost:3306/laundry-tubes";
+
+    public Connection makeConnection() {
+        System.out.println("Opening Database...");
+        try {
+            CON = DriverManager.getConnection(URL + PATH, "root", "");
+            System.out.println("Success!");
+        } catch (Exception e) {
+            System.out.println("Error opening database");
+            System.out.println(e);
+        }
+        return CON;
+    }
     
+    public void closeConnection(){
+       System.out.println("Closing database..");
+       try{
+           CON.close();
+           System.out.println("Success!");
+       }catch (Exception e){
+           System.out.println("Error closing database");
+           System.out.println(e);
+       }
+   }
 }
