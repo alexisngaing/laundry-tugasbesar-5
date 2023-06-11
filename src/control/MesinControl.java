829c4cee03290e0ab7a9dbfd24fccfe58977f7e4
HP <HP@DESKTOP-HT4HO5T>
2023-06-11 21:43:53 +0700

    Mesin Control

--- src/control/MesinControl.java
+++ src/control/MesinControl.java
@@ -1,10 +1,36 @@
 package control;
 
+import model.Mesin;
 import dao.MesinDAO;
 import java.util.List;
-import model.Mesin;
-//import table.TableMesin;
 
 public class MesinControl {
+    private MesinDAO mesinDAO = new MesinDAO();
 
+    public void insertMesin(Mesin mesin) {
+        mesinDAO.insertMesin(mesin);
+    }
+
+    public String showMesin() {
+        List<Mesin> dataMesin = mesinDAO.showMesin();
+        String mesinString = "";
+        for (int i = 0; i < dataMesin.size(); i++) {
+            mesinString = mesinString + dataMesin.get(i).toString() + "\n";
+        }
+        return mesinString;
+    }
+
+    public Mesin searchMesin(int idMesin) {
+        Mesin mesin = mesinDAO.searchMesin(idMesin);
+        return mesin;
+    }
+
+    public void updateMesin(Mesin mesin) {
+        mesinDAO.updateMesin(mesin);
+    }
+
+    public void deleteMesin(int idMesin) {
+        mesinDAO.deleteMesin(idMesin);
+    }
 }
+
