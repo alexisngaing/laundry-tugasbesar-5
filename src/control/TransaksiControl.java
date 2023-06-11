1e12aff810d65010fb341225d5ead7f0b3b5817d
HP <HP@DESKTOP-HT4HO5T>
2023-06-11 21:43:34 +0700

    Transaksi Control

--- src/control/TransaksiControl.java
+++ src/control/TransaksiControl.java
@@ -1,4 +1,35 @@
 package control;
 
+import model.Transaksi;
+import dao.TransaksiDAO;
+import java.util.List;
+
 public class TransaksiControl {
+    private TransaksiDAO transaksiDAO = new TransaksiDAO();
+
+    public void insertDataTransaksi(Transaksi transaksi) {
+        transaksiDAO.insertTransaksi(transaksi);
+    }
+
+    public String showTransaksi() {
+        List<Transaksi> dataTransaksi = transaksiDAO.showTransaksi();
+        String transaksiString = "";
+        for (int i = 0; i < dataTransaksi.size(); i++) {
+            transaksiString = transaksiString + dataTransaksi.get(i).toString() + "\n";
+        }
+        return transaksiString;
+    }
+
+    public Transaksi searchTransaksi(int idTransaksi) {
+        Transaksi transaksi = transaksiDAO.searchTransaksi(idTransaksi);
+        return transaksi;
+    }
+
+    public void updateTransaksi(Transaksi transaksi) {
+        transaksiDAO.updateTransaksi(transaksi);
+    }
+
+    public void deleteTransaksi(int idTransaksi) {
+        transaksiDAO.deleteTransaksi(idTransaksi);
+    }
 }
