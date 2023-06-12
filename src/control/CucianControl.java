982843d71ba08c135c777056329b0b0fd4e9148d
Adit <Adit@DESKTOP-ABPQAVE>
2023-06-12 20:03:58 +0700

    cucian control

--- src/control/CucianControl.java
+++ src/control/CucianControl.java
@@ -1,4 +1,39 @@
 package control;
 
+import model.Cucian;
+import dao.CucianDAO;
+import java.util.List;
+import table.TableCucian;
+
 public class CucianControl {
+    private CucianDAO cucianDAO = new CucianDAO();
+
+    public void insertDataCucian(Cucian cucian) {
+        cucianDAO.insertCucian(cucian);
+    }
+
+    public TableCucian showCucian() {
+        List<Cucian> dataCucian = cucianDAO.showCucian();
+        TableCucian tableCucian = new TableCucian(dataCucian);
+//        String cucianString = "";
+//        for (int i = 0; i < dataCucian.size(); i++) {
+//            cucianString = cucianString + dataCucian.get(i).toString() + "\n";
+//        }
+        return tableCucian;
+    }
+
+    public Cucian searchCucian(int idCucian) {
+        Cucian cucian = cucianDAO.searchCucian(idCucian);
+        return cucian;
+    }
+
+    public void updateCucian(Cucian cucian) {
+        cucianDAO.updateCucian(cucian);
+    }
+
+    public void deleteCucian(int idCucian) {
+        cucianDAO.deleteCucian(idCucian);
+    }
 }
+
+
