a72b17ecf241963ab223f37934076ce6336ab511
HP <HP@DESKTOP-HT4HO5T>
2023-06-12 15:08:51 +0700

    exception

---
+++ src/exception/inputKosongException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author HP
+ */
+public class inputKosongException extends Exception{
+    public String message(){
+        return "FIELD INPUT TIDAK BOLEH KOSONG !";
+    }
+    
+}
