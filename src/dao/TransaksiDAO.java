5dfdf442f294a873253cc1dcc21ce4a8084e0d92
HP <HP@DESKTOP-HT4HO5T>
2023-06-13 15:31:57 +0700

    edit transaksiDAO

--- src/dao/TransaksiDAO.java
+++ src/dao/TransaksiDAO.java
@@ -6,23 +6,23 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import model.Transaksi;
 import model.Pelanggan;
 import model.Cucian;
+import model.Transaksi;
 
 public class TransaksiDAO {
     private DbConnection dbCon = new DbConnection();
     private Connection con;
-
-    public void insertTransaksi(Transaksi transaksi) {
+    
+    public void insertTransaksi(Transaksi t){
         con = dbCon.makeConnection();
-
-        String sql = "INSERT INTO transaksi(idTransaksi, totalBiaya, idPelanggan, idCucian) "
-                + "VALUES (" + transaksi.getIdTransaksi() + ", " + transaksi.getTotalBiaya() + ", "
-                + transaksi.getPelanggan().getIdPelanggan()+ ", " + transaksi.getCucian().getId() + ")";
-
+        
+        String sql = "INSERT INTO transaksi(totalBiaya, idPelanggan, idCucian) "
+                + "VALUES ('" + t.getTotalBiaya() + "', '" + t.getPelanggan().getIdPelanggan() + "', '"
+                + t.getCucian().getId() + "')";
+        
         System.out.println("Adding Transaksi");
-
+        
         try {
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
@@ -31,35 +31,51 @@ public class TransaksiDAO {
         } catch (Exception e) {
             System.out.println("[!] Error Adding Transaksi");
             System.out.println(e);
-        }
+        }   
     }
-
+    
     public List<Transaksi> showTransaksi() {
         con = dbCon.makeConnection();
-
-        String sql = "SELECT * FROM transaksi";
+        
+        String sql = "SELECT t.*, p.*, c.* FROM transaksi as t "
+                + "JOIN pelanggan as p ON t.idPelanggan = p.id "
+                + "JOIN cucian as c ON t.idCucian = c.id";
+        
         System.out.println("Fetching Transaksi data...");
 
         List<Transaksi> list = new ArrayList<>();
-
+        
         try {
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sql);
 
             if (rs != null) {
                 while (rs.next()) {
-                    int idTransaksi = rs.getInt("idTransaksi");
-                    float totalBiaya = rs.getFloat("totalBiaya");
-                    int idPelanggan = rs.getInt("idPelanggan");
-                    int idCucian = rs.getInt("idCucian");
-
-                    PelangganDAO pelangganDAO = new PelangganDAO();
-                    Pelanggan pelanggan = pelangganDAO.searchPelanggan(sql);
-
-                    CucianDAO cucianDAO = new CucianDAO();
-                    Cucian cucian = cucianDAO.searchCucian(idCucian);
-
-                    Transaksi transaksi = new Transaksi(idTransaksi, totalBiaya, pelanggan, cucian);
+                    Pelanggan pelanggan = new Pelanggan(
+                            rs.getInt("p.id"),
+                            rs.getString("p.nama"),
+                            rs.getString("p.noTelp"),
+                            rs.getString("p.alamat")
+                    );
+                    
+                    Cucian cucian = new Cucian(
+                            rs.getInt("c.id"),
+                            rs.getBoolean("c.statusCuci"),
+                            rs.getBoolean("c.statusDry"),
+                            rs.getFloat("c.berat"),
+                            rs.getString("c.tglMasuk"),
+                            rs.getString("c.tglKeluar"),
+                            null,
+                            pelanggan
+                    );
+                    
+                    Transaksi transaksi = new Transaksi(
+                            rs.getInt("t.idTransaksi"),
+                            rs.getFloat("t.totalBiaya"),
+                            pelanggan,
+                            cucian
+                    );
+                    
                     list.add(transaksi);
                 }
             }
@@ -70,32 +86,53 @@ public class TransaksiDAO {
         dbCon.closeConnection();
         return list;
     }
-
-    public Transaksi searchTransaksi(int idTransaksi) {
+    
+    public Transaksi searchTransaksi(int idTransaksi){
         con = dbCon.makeConnection();
-
-        String sql = "SELECT * FROM transaksi WHERE idTransaksi = " + idTransaksi;
+        
+        String sql = "SELECT t.*, p.*, c.* FROM transaksi as t "
+                + "JOIN pelanggan as p ON t.idPelanggan = p.id "
+                + "JOIN cucian as c ON t.idCucian = c.id "
+                + "WHERE t.idTransaksi = " + idTransaksi;
+        
         System.out.println("Searching Transaksi...");
-
+        
         Transaksi transaksi = null;
-
+        
         try {
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sql);
-
-            if (rs != null && rs.next()) {
-                float totalBiaya = rs.getFloat("totalBiaya");
-                int idPelanggan = rs.getInt("idPelanggan");
-                int idCucian = rs.getInt("idCucian");
-
-                PelangganDAO pelangganDAO = new PelangganDAO();
-                Pelanggan pelanggan = pelangganDAO.searchPelanggan("nama");
-
-                CucianDAO cucianDAO = new CucianDAO();
-                Cucian cucian = cucianDAO.searchCucian(idCucian);
-
-                transaksi = new Transaksi(idTransaksi, totalBiaya, pelanggan, cucian);
+            
+            if (rs != null ) {
+                while (rs.next()) {
+                    Pelanggan pelanggan = new Pelanggan(
+                            rs.getInt("p.id"),
+                            rs.getString("p.nama"),
+                            rs.getString("p.noTelp"),
+                            rs.getString("p.alamat")
+                    );
+                    
+                    Cucian cucian = new Cucian(
+                            rs.getInt("c.id"),
+                            rs.getBoolean("c.statusCuci"),
+                            rs.getBoolean("c.statusDry"),
+                            rs.getFloat("c.berat"),
+                            rs.getString("c.tglMasuk"),
+                            rs.getString("c.tglKeluar"),
+                            null,
+                            pelanggan
+                    );
+                    
+                    transaksi = new Transaksi(
+                            rs.getInt("t.idTransaksi"),
+                            rs.getFloat("t.totalBiaya"),
+                            pelanggan,
+                            cucian
+                    );
+                }       
             }
+            rs.close();
+            statement.close();
         } catch (Exception e) {
             System.out.println("[!] Error Searching Transaksi...");
             System.out.println(e);
@@ -104,43 +141,46 @@ public class TransaksiDAO {
 
         return transaksi;
     }
-
-    public void updateTransaksi(Transaksi transaksi) {
+    
+    public void updateTransaksi(Transaksi t){
         con = dbCon.makeConnection();
-
-        String sql = "UPDATE transaksi SET totalBiaya = " + transaksi.getTotalBiaya()
-                + ", idPelanggan = " + transaksi.getPelanggan().getIdPelanggan()
-                + ", idCucian = " + transaksi.getCucian().getId()
-                + " WHERE idTransaksi = " + transaksi.getIdTransaksi();
-
+        
+        String sql = "UPDATE transaksi SET "
+                + "totalBiaya = '" + t.getTotalBiaya() + "', "
+                + "idPelanggan = " + t.getPelanggan().getIdPelanggan() + ", "
+                + "idCucian = " + t.getCucian().getId() + " "
+                + "WHERE idTransaksi = " + t.getIdTransaksi();
+        
         System.out.println("Updating Transaksi...");
-
+        
         try {
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
-            System.out.println("Updated " + result + " Transaksi");
+            System.out.println("Updated " + result + " Transaksi " + t.getIdTransaksi());
             statement.close();
         } catch (Exception e) {
-            System.out.println("[!] Error Updating Transaksi...");
+            System.out.println("[!] Error updating Transaksi...");
             System.out.println(e);
         }
     }
-
+    
     public void deleteTransaksi(int idTransaksi) {
         con = dbCon.makeConnection();
 
         String sql = "DELETE FROM transaksi WHERE idTransaksi = " + idTransaksi;
+        
         System.out.println("Deleting Transaksi...");
-
+        
         try {
             Statement statement = con.createStatement();
             int result = statement.executeUpdate(sql);
-            System.out.println("Deleted " + result + " Transaksi");
+            System.out.println("Deleted " + result + " Transaksi " + idTransaksi);
             statement.close();
         } catch (Exception e) {
-            System.out.println("[!] Error Deleting Transaksi...");
+            System.out.println("[!] Error deleting Transaksi...");
             System.out.println(e);
         }
+        
         dbCon.closeConnection();
     }
 }
