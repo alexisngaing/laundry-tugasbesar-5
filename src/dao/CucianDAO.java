05f591b51129a8da3a2b8db862dc4d096353b7d5
Stefanus Vemas <stefanusvemas@gmail.com>
2023-06-13 23:02:56 +0700

    Fix for showCucian

--- src/dao/CucianDAO.java
+++ src/dao/CucianDAO.java
@@ -43,12 +43,12 @@ public class CucianDAO {
         
         String sql = "SELECT c.*, p.*, m.* FROM cucian as c JOIN mesin as m ON c.idMesin = m.id JOIN pelanggan as p ON c.idPelanggan = p.id WHERE (c.id LIKE "
                 + "'%" + query + "%'"
-                + "OR c.statusCuci = '" + bool + "'"
+                + "AND c.statusCuci = '" + bool + "'"
 //                + "OR c.statusDry LIKE '%" + query + "%'"
-                + "OR c.berat LIKE '%" + query + "%'"
-                + "OR c.tglMasuk LIKE '%" + query + "%'"
-                + "OR m.id LIKE '%" + query + "%'"
-                + "OR p.nama LIKE '%" + query + "%')";
+                + "AND c.berat LIKE '%" + query + "%'"
+                + "AND c.tglMasuk LIKE '%" + query + "%'"
+                + "AND m.id LIKE '%" + query + "%'"
+                + "AND p.nama LIKE '%" + query + "%')";
         
         System.out.println("Mengambil data Cucian...");
         System.out.println(sql);
